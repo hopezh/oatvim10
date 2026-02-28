5597e2392f2ab2aa9548b06eb1fc154821cf683d
Ji ZHANG <hope.zh@gmail.com>
2026-02-28 18:59:14 +0800

    add option to change shell based on OS

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -23,7 +23,7 @@ opt.undofile = false -- stop unlimited undo to even changes made in previous ses
 
 -- blinking cursor -------------------------------------------------------------
 opt.guicursor =
-  "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+    "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 -- no swap file ----------------------------------------------------------------
 vim.cmd("set noswapfile")
@@ -38,3 +38,22 @@ vim.scriptencoding = "utf-8"
 --  * enables nerd font icons
 -- Set to false to disable.
 vim.g.lazygit_config = false
+
+-- switch shells based on OS ---------------------------------------------------
+-- Detect OS
+local is_win = vim.loop.os_uname().sysname:find("Windows") ~= nil
+local is_mac = vim.loop.os_uname().sysname == "Darwin"
+if is_win then
+    -- Use PowerShell 7 (pwsh) if available, otherwise fallback to Windows PowerShell
+    local powershell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
+    vim.opt.shell = powershell
+    vim.opt.shellcmdflag =
+        "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;$PSStyle.Formatting.Error = '';$PSStyle.Formatting.ErrorAccent = '';$PSStyle.Formatting.Warning = '';$PSStyle.OutputRendering = 'PlainText';"
+    vim.opt.shellredir = "2>&1 | Out-File -Encoding utf8 %s; exit $LastExitCode"
+    vim.opt.shellpipe = "2>&1 | Out-File -Encoding utf8 %s; exit $LastExitCode"
+    vim.opt.shellquote = ""
+    vim.opt.shellxquote = ""
+elseif is_mac then
+    -- Explicitly set zsh for macOS
+    vim.opt.shell = "zsh"
+end
